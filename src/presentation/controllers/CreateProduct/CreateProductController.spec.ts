14772934d30ba7a6c9c1d81cde09bc76c2bb30f1
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 17:44:16 -0300

    test: ensure CreateProductController returns 400 if no title is provided

---
+++ src/presentation/controllers/CreateProduct/CreateProductController.spec.ts
@@ -0,0 +1,18 @@
+import { CreateProductController } from './CreateProductController';
+import { fakeProductRequest } from './constants';
+import { badRequestResponse } from '../../helpers';
+import { MissingParamError } from '../../errors';
+
+let createProduct_sut: CreateProductController;
+
+describe('CreateProduct Controller', () => {
+    beforeAll(() => {
+        createProduct_sut = new CreateProductController();
+    })
+
+    test('Should return 400 if no title is provided', async () => {
+        const response = await createProduct_sut.handle(fakeProductRequest.withoutTitle);
+
+        expect(response).toEqual(badRequestResponse(new MissingParamError('title')));
+    });
+});
