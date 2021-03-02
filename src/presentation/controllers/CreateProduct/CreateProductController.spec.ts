0778d8571f1c51451ed8294380cd6d309b59bb1d
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 19:24:40 -0300

    test: ensure CreateProductController returns 400 if price is not a number

--- src/presentation/controllers/CreateProduct/CreateProductController.spec.ts
+++ src/presentation/controllers/CreateProduct/CreateProductController.spec.ts
@@ -1,13 +1,25 @@
 import { CreateProductController } from './CreateProductController';
 import { fakeProductRequest } from './constants';
 import { badRequestResponse } from '../../helpers';
-import { MissingParamError } from '../../errors';
+import { InvalidParamError, MissingParamError } from '../../errors';
+import { ICreateProduct, ICreateProductRequest } from '../../../domain/useCases/ICreateProduct';
+import { IProduct } from '../../../domain/models/IProduct';
 
 let createProduct_sut: CreateProductController;
+let fakeCreateProductUseCase: FakeCreateProductUseCase;
+
+class FakeCreateProductUseCase implements ICreateProduct {
+    public async create(request: ICreateProductRequest): Promise<IProduct> {
+        const productWithId = Object.assign({}, request, { id: 'valid_id' });
+
+        return new Promise(resolve => resolve(productWithId));
+    }
+}
 
 describe('CreateProduct Controller', () => {
     beforeAll(() => {
-        createProduct_sut = new CreateProductController();
+        fakeCreateProductUseCase = new FakeCreateProductUseCase();
+        createProduct_sut = new CreateProductController(fakeCreateProductUseCase);
     })
 
     test('Should return 400 if no title is provided', async () => {
@@ -33,11 +45,8 @@ describe('CreateProduct Controller', () => {
         expect(responsePriceZero).toEqual(badRequestResponse(new MissingParamError('price')));
     });
 
-    test('Should return 400 if no price is provided or price is zero', async () => {
-        const responseWithouPrice = await createProduct_sut.handle(fakeProductRequest.withoutPrice);
-        expect(responseWithouPrice).toEqual(badRequestResponse(new MissingParamError('price')));
-        
-        const responsePriceZero = await createProduct_sut.handle(fakeProductRequest.priceIsZero);
-        expect(responsePriceZero).toEqual(badRequestResponse(new MissingParamError('price')));
+    test('Should return 400 if no price is string', async () => {
+        const response = await createProduct_sut.handle(fakeProductRequest.priceIsString);
+        expect(response).toEqual(badRequestResponse(new InvalidParamError('preço não é tipo number.')));
     });
 });
