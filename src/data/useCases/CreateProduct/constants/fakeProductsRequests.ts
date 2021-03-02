1679ee682de0d5d5620e7ebb523daf9b61b15701
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 18:37:54 -0300

    feat: create pproductRequests for createProducts useCases data layer

---
+++ src/data/useCases/CreateProduct/constants/fakeProductsRequests.ts
@@ -0,0 +1,8 @@
+export const fakeProductRequest = {
+    valid: {
+        title: 'valid_title',
+        description: 'valid_description',
+        categoryId: 'valid_categoryId',
+        price: 10
+    }
+}
