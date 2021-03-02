7b526e3ddc5d645ae0a7019aef4903acfde38411
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 19:41:52 -0300

    feat: create routeAdapter helper method

---
+++ src/main/adapters/routeAdapter.ts
@@ -0,0 +1,14 @@
+import { IRequest, IResponse } from '../protocols/IExpress'
+import { IController, IHttpRequest } from '../../presentation/protocols'
+
+export const adaptRoute = (controller: IController) => {
+    return async (req: IRequest, res: IResponse) => {
+        const httpRequest: IHttpRequest = {
+            body: req.body
+        }
+
+        const httpResponse = await controller.handle(httpRequest)
+
+        res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+}
