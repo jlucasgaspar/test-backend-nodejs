7058d02984eb00a5d88a270acb43c90a0a84b574
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 18:56:57 -0300

    feat: add helper methods to mongodb infra layer

---
+++ src/infra/db/mongodb/helpers/mongoHelper.ts
@@ -0,0 +1,34 @@
+import { Collection, MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+    client: null as MongoClient,
+    uri: null as string,
+
+    async connect(uri: string): Promise<void> {
+        this.uri = uri
+
+        this.client = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    },
+
+    async disconnect(): Promise<void> {
+        await this.client.close()
+        this.client = null
+    },
+
+    async getCollection(name: string): Promise<Collection> {
+        if (!this.client?.isConnected()) {
+            await this.connect(this.uri)
+        }
+
+        return this.client.db().collection(name)
+    },
+
+    map(collection: any): any {
+        const { _id, ...collectionWithout_id } = collection
+    
+        return Object.assign({}, collectionWithout_id, { id: _id })
+    }
+}
