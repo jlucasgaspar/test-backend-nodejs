eb50d0d34fc7c4d28654792e1821637603171a61
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 19:10:14 -0300

    chore: add @shelf/jest-mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  preset: '@shelf/jest-mongodb',
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
