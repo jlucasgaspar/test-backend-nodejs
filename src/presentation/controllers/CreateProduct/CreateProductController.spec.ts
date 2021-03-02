428409c1ea2bf6d82806257f4a67b01a1fc389f9
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 17:51:46 -0300

    test: ensure CreateProductController returns 400 if no price is provided

--- src/presentation/controllers/CreateProduct/CreateProductController.spec.ts
+++ src/presentation/controllers/CreateProduct/CreateProductController.spec.ts
@@ -19,4 +19,25 @@ describe('CreateProduct Controller', () => {
         const response = await createProduct_sut.handle(fakeProductRequest.withoutCategoryId);
         expect(response).toEqual(badRequestResponse(new MissingParamError('categoryId')));
     });
+    
+    test('Should return 400 if no description is provided', async () => {
+        const response = await createProduct_sut.handle(fakeProductRequest.withoutDescription);
+        expect(response).toEqual(badRequestResponse(new MissingParamError('description')));
+    });
+
+    test('Should return 400 if no price is provided or price is zero', async () => {
+        const responseWithouPrice = await createProduct_sut.handle(fakeProductRequest.withoutPrice);
+        expect(responseWithouPrice).toEqual(badRequestResponse(new MissingParamError('price')));
+        
+        const responsePriceZero = await createProduct_sut.handle(fakeProductRequest.priceIsZero);
+        expect(responsePriceZero).toEqual(badRequestResponse(new MissingParamError('price')));
+    });
+
+    test('Should return 400 if no price is provided or price is zero', async () => {
+        const responseWithouPrice = await createProduct_sut.handle(fakeProductRequest.withoutPrice);
+        expect(responseWithouPrice).toEqual(badRequestResponse(new MissingParamError('price')));
+        
+        const responsePriceZero = await createProduct_sut.handle(fakeProductRequest.priceIsZero);
+        expect(responsePriceZero).toEqual(badRequestResponse(new MissingParamError('price')));
+    });
 });
