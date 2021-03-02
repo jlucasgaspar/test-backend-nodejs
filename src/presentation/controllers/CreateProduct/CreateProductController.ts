e09fb9c5fec2874351b9b03b8e7093dd050af40c
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 19:24:33 -0300

    feat: ensure CreateProductController returns 400 if price is not a number

--- src/presentation/controllers/CreateProduct/CreateProductController.ts
+++ src/presentation/controllers/CreateProduct/CreateProductController.ts
@@ -1,8 +1,15 @@
-import { MissingParamError } from '../../errors';
+import { MissingParamError, InvalidParamError } from '../../errors';
 import { badRequestResponse, ensureRequiredFieldsAreNotEmpty } from '../../helpers';
 import { IController, IHttpRequest, IHttpResponse } from '../../protocols';
+import { ICreateProduct } from '../../../domain/useCases/ICreateProduct';
 
 export class CreateProductController implements IController {
+    private readonly createProduct: ICreateProduct;
+
+    constructor(createProduct: ICreateProduct) {
+        this.createProduct = createProduct;
+    }
+
     public async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
         const requiredFields = ['title', 'categoryId', 'description', 'price']
 
@@ -15,9 +22,13 @@ export class CreateProductController implements IController {
             return badRequestResponse(new MissingParamError(emptyField));
         }
 
-        const { title, description, categoryId, price } = httpRequest.body
+        const { title, description, categoryId, price } = httpRequest.body;
 
-        
+        if (typeof price !== 'number') {
+            return badRequestResponse(new InvalidParamError('preço não é tipo number.'))
+        }
+
+        const product = await this.createProduct.create({ title, description, categoryId, price });
 
         return new Promise(resolve => resolve({ statusCode: 200, body: null }))
     }
