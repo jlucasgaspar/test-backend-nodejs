bbf964044c79c717027f7133b0b010df15e2e98a
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 17:27:51 -0300

    feat: add Product and Category models interfaces

--- presentation/controllers/protocols/IController.ts
+++
@@ -1,5 +0,0 @@
-import { IHttpRequest, IHttpResponse } from './IHttp';
-
-export interface IController {
-    handle(request: IHttpRequest): Promise<IHttpResponse>;
-}
---
+++ src/domain/models/IProduct.ts
@@ -0,0 +1,7 @@
+export interface IProduct {
+    id: string;
+    categoryId: string;
+    title: string;
+    description: string;
+    price: number;
+}
