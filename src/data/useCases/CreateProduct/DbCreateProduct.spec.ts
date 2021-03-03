90eac63bb5badcd373ab0f9e6b903210c9fc2218
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-03-02 21:20:40 -0300

    test: ensure DbCreateProduct returns an error if we try to save a product with a name that already exists

--- src/data/useCases/CreateProduct/DbCreateProduct.spec.ts
+++ src/data/useCases/CreateProduct/DbCreateProduct.spec.ts
@@ -10,6 +10,7 @@ let fakeProductsRepository: IProductsRepository;
 let dbCreateProduct_sut: DbCreateProduct;
 
 class FakeProductsRepository implements IProductsRepository {
+    private readonly fakeArray: Array<IProduct | null> = [];
     private readonly mongodbProductRepositoy: MongoProductsRepository;
 
     constructor(mongodbProductRepositoy: MongoProductsRepository) {
@@ -19,12 +20,26 @@ class FakeProductsRepository implements IProductsRepository {
     public async save(productData: ICreateProductRequest): Promise<IProduct> {
         const productWithId = Object.assign({}, productData, { id: 'valid_id' });
 
+        this.fakeArray.push(productWithId);
+
         return new Promise(resolve => resolve(productWithId));
+    }
+
+    public async getByTitle(title: string): Promise<IProduct | null> {
+        const productExists = this.fakeArray.find(product => {
+            return product.title === title
+        })
+
+        if (productExists) {
+            throw new Error()
+        }
+
+        return new Promise(resolve => resolve(null));
     }
 }
 
 describe('DbCreateProduct UseCase', () => {
-    beforeAll(() => {
+    beforeEach(() => {
         mongodbProductRepositoy = new MongoProductsRepository();
         fakeProductsRepository = new FakeProductsRepository(mongodbProductRepositoy);
         dbCreateProduct_sut = new DbCreateProduct(fakeProductsRepository);
@@ -35,6 +50,13 @@ describe('DbCreateProduct UseCase', () => {
         await dbCreateProduct_sut.create(fakeProductRequest.valid);
         expect(saveSpy).toHaveBeenCalledWith(fakeProductRequest.valid);
     });
+
+    test('should throw an error if product title already exists', async () => {
+        await dbCreateProduct_sut.create(fakeProductRequest.valid);
+        const promise = dbCreateProduct_sut.create(fakeProductRequest.valid);
+        await expect(promise).rejects.toThrow();
+    });
+    
 
     test('should throw if ProductsRepository throws', async () => {
         jest.spyOn(fakeProductsRepository, 'save')
